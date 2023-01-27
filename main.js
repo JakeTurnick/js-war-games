f92b4c5c4fbe948ae507a57e0ef433ed750b4efc
JakeTurnick <jaketurnick@gmail.com>
2023-01-26 22:10:29 -0500

    game now functions off score to win - featue complete

--- main.js
+++ main.js
@@ -1,79 +1,78 @@
 // import game from './modules/game.js';
 // import { Deck } from './modules/deck.js';
-import {Game} from  './modules/game.js'
+import { Game } from "./modules/game.js";
 
 //on Screen game elements
-const btnDraw = document.getElementById("draw")
-const btnStart = document.getElementById("start")
-const btnRestart = document.getElementById("restart")
-const roundInfo = document.getElementById("round-info")
+const btnDraw = document.getElementById("draw");
+const btnStart = document.getElementById("start");
+const btnRestart = document.getElementById("restart");
+const roundInfo = document.getElementById("round-info");
 //Player 1
 const p1Card = document.querySelector(".p1.card");
 const p1Rank = document.querySelector(".p1.rank");
 const p1Suit = document.querySelector(".p1.suit");
 const p1Hand = document.querySelector(".p1.hand");
+const p1Score = document.querySelector(".p1.score");
 //Player2
 const p2Card = document.querySelector(".p2.card");
 const p2Rank = document.querySelector(".p2.rank");
 const p2Suit = document.querySelector(".p2.suit");
 const p2Hand = document.querySelector(".p2.hand");
-
+const p2Score = document.querySelector(".p2.score");
 
 let p1PlayRank;
 let p1PlaySuit;
 let p2PlayRank;
 let p2PlaySuit;
 
-btnDraw.addEventListener('click', () => {
-    console.log('the draw:', currentGame.draw())
+btnDraw.addEventListener("click", () => {
+  console.log("the draw:", currentGame.draw());
 
-    roundInfo.innerHTML = `Round winner: ${currentGame.roundWin}`;
-    if (currentGame.winner) {
-        roundInfo.innerHTML = `We have a winner!\n Congradulations ${currentGame.winner}`;
-    }
+  roundInfo.innerHTML = `Round winner: ${currentGame.roundWin}`;
+  if (currentGame.winner) {
+    roundInfo.innerHTML = `We have a winner!\n Congradulations ${currentGame.winner}`;
+  }
 
-    p1PlayRank = currentGame.player1.play.rank;
-    p1PlaySuit = currentGame.player1.play.suit.toLowerCase();
-    p2PlayRank = currentGame.player2.play.rank;
-    p2PlaySuit = currentGame.player2.play.suit.toLowerCase();
-    
-    p1Rank.innerHTML = p1PlayRank;
-    p2Rank.innerHTML = p2PlayRank;
+  p1PlayRank = currentGame.player1.play.rank;
+  p1PlaySuit = currentGame.player1.play.suit.toLowerCase();
+  p2PlayRank = currentGame.player2.play.rank;
+  p2PlaySuit = currentGame.player2.play.suit.toLowerCase();
 
+  p1Rank.innerHTML = p1PlayRank;
+  p2Rank.innerHTML = p2PlayRank;
 
-    p1Suit.src = `./imgs/suits/${p1PlaySuit}.png`;
-    p2Suit.src = `./imgs/suits/${p2PlaySuit}.png`;
+  p1Suit.src = `./imgs/suits/${p1PlaySuit}.png`;
+  p2Suit.src = `./imgs/suits/${p2PlaySuit}.png`;
 
-    p1Hand.innerHTML = `P1 Hand: ${currentGame.player1.hand.length}`;
-    p2Hand.innerHTML = `P2 Hand: ${currentGame.player2.hand.length}`;
-
-})
+  p1Hand.innerHTML = `P1 Hand: ${currentGame.player1.hand.length}`;
+  p1Score.innerHTML = `P1 Score: ${currentGame.player1.score}`;
+  p2Hand.innerHTML = `P2 Hand: ${currentGame.player2.hand.length}`;
+  p2Score.innerHTML = `P2 Score: ${currentGame.player2.score}`;
+});
 
 let i = 0;
 let totalGames = [];
-let currentGame
+let currentGame;
 
-btnStart.addEventListener('click', () => {
-    totalGames.push(new Game())
-    currentGame = totalGames[i];
-    i++;
-    currentGame.shuffleDeck();
-    currentGame.deal();
-    console.log('total games: ', totalGames)
-    console.log('current game: ', currentGame)
-    // let newGame = new Game;
-    // console.log(newGame)
-    // newGame.shuffleDeck()
-    // newGame.deal();
+btnStart.addEventListener("click", () => {
+  totalGames.push(new Game());
+  currentGame = totalGames[i];
+  i++;
+  currentGame.shuffleDeck();
+  currentGame.deal();
+  console.log("total games: ", totalGames);
+  console.log("current game: ", currentGame);
+  // let newGame = new Game;
+  // console.log(newGame)
+  // newGame.shuffleDeck()
+  // newGame.deal();
 
-    roundInfo.innerHTML = 'New game'
-    p1Rank.innerHTML = '0';
-    p2Rank.innerHTML = '0';
-    p1Suit.src = ' ';
-    p2Suit.src = ' ';
+  roundInfo.innerHTML = "New game";
+  p1Rank.innerHTML = "0";
+  p2Rank.innerHTML = "0";
+  p1Suit.src = " ";
+  p2Suit.src = " ";
 
-    p1Hand.innerHTML = `P1 Hand: ${currentGame.player1.hand.length}`;
-    p2Hand.innerHTML = `P2 Hand: ${currentGame.player2.hand.length}`;
-
-    
+  p1Hand.innerHTML = `P1 Hand: ${currentGame.player1.hand.length}`;
+  p2Hand.innerHTML = `P2 Hand: ${currentGame.player2.hand.length}`;
 });
--- modules/player.js
+++ modules/player.js
@@ -1,14 +1,12 @@
-
 function Player(hand = [], setScore = 0, play = []) {
-    this.hand = hand;
-    this.scorePile = [];
-    this.setScore = setScore;
-    this.play = play
-    //Hand - current cards available to play
-    //ScorePile - cards won so far (if scorePile.length >= 52 - that player wins)
-    //setScore - victory count, how many games won in a set
+  this.hand = hand;
+  this.scorePile = [];
+  this.score = 0;
+  this.setScore = setScore;
+  this.play = play;
+  //Hand - current cards available to play
+  //ScorePile - cards won so far (if scorePile.length >= 52 - that player wins)
+  //setScore - victory count, how many games won in a set
 }
 
-
-
-export {Player};
+export { Player };
